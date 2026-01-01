8e853f41bf34b77e977f65c4811c7e4ea9455131
Mustafa Bite <mustafabite2357@gmail.com>
2026-01-01 17:15:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using VibeMap.DataAccess;
+using VibeMap.Forms;
+
+namespace VibeMap
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                DatabaseInitializer.Initialize();
+
+                Application.Run(new FrmLogin());
+            }
+            catch (Exception ex)
+            {
+                string errorMsg = "KRİTİK HATA: " + ex.Message + "\n\n" + ex.StackTrace;
+                if (ex.InnerException != null)
+                {
+                    errorMsg += "\n\nİç Hata: " + ex.InnerException.Message + "\n" + ex.InnerException.StackTrace;
+                }
+                System.IO.File.WriteAllText("startup_error.log", errorMsg);
+                MessageBox.Show(errorMsg, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
